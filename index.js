99d720191d7849ed8b1ba9a85df8e02e85f4f5b0
Katherine Moreira <kmoreira17_93@hotmail.com>
2025-07-28 21:34:00 -0600

    change: files c3

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log('Hello, World!');
 console.log('Cambio 2')
+console.log('Cambio 3')
--- index2.js
+++ index2.js
@@ -1,2 +1,3 @@
 console.log('Hello, World! from index2.js');
 console.log('Cambio 2')
+console.log('Cambio 3')
